c35cacb8ed1f2875c2ea9dceb525afe38da74575
sanidhya-01 <sanidhyachauhan2004@gmail.com>
2025-07-01 10:53:17 +0530

    Adding Opertor

---
+++ ObjectCreation.java
@@ -0,0 +1,20 @@
+// OBJECT CREATION
+public class ObjectCreation {
+
+
+        public void myMethod()
+        {
+            int x=1;
+            int y=2;
+            int a=x+y;
+            System.out.println(a);
+        }
+        public static void main(String[]args){
+            System.out.println("Sanidhya");
+            ObjectCreation m=new ObjectCreation();
+            m.myMethod();
+        }
+    }
+
+
+
