d3489f640ca3cbc976e3718b43dbc71325422c47
sanidhya-01 <sanidhyachauhan2004@gmail.com>
2025-07-02 12:55:48 +0530

    Adding

---
+++ FirstProgram.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FirstProgram
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+String x=sc.nextLine();
+int y=sc.nextInt();
+System.out.println(x);
+System.out.print(y);
+}
+}
