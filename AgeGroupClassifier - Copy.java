a7cd20e17a3e18d85cae9e670e371306b4bdfd7b
sanidhya-01 <sanidhyachauhan2004@gmail.com>
2025-07-07 12:06:33 +0530

    Adding

---
+++ AgeGroupClassifier - Copy.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AgeGroupClassifier {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input age
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        // Classify age group
+        if (age >= 0 && age < 13) {
+            System.out.println("Child");
+        } else if (age >= 13 && age < 18) {
+            System.out.println("Teenager");
+        } else if (age >= 18 && age <= 30) {
+            System.out.println("Adult");
+        } else if (age > 30) {
+            System.out.println("Old");
+        } else {
+            System.out.println("Invalid age");
+        }
+
+        scanner.close();
+}
+}
---
+++ SwitchCase1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SwitchCase1{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number:");
+        int x=sc.nextInt();
+        switch(x){
+            case 1:System.out.print(50000*30/100+50000);
+                break;
+            case 2:System.out.print(40000*25/100+40000);
+                break;
+            case 3:System.out.print(30000*20/100+30000);
+                break;
+            case 4:System.out.print(20000*15/100+20000);
+                break;
+ }
+}
+}
